5e074f66375d367d4860a54ada42891746f29397
Antony Blakey <antony.blakey@gmail.com>
2024-06-16 21:15:16 +0000

    Allow cli in slang_solidity to be disabled

--- crates/solidity/outputs/cargo/slang_solidity/src/main.rs
+++ crates/solidity/outputs/cargo/slang_solidity/src/main.rs
@@ -1,17 +1,20 @@
-use std::fs;
-use std::path::PathBuf;
-use std::process::ExitCode;
-
-use anyhow::{Context, Result};
-use clap::{Parser as ClapParser, Subcommand};
-use semver::Version;
-use slang_solidity::kinds::NonterminalKind;
-use slang_solidity::language::Language;
+#[cfg(feature = "cli")]
+use {
+    anyhow::{Context, Result},
+    clap::{Parser as ClapParser, Subcommand},
+    semver::Version,
+    slang_solidity::kinds::NonterminalKind,
+    slang_solidity::language::Language,
+    std::fs,
+    std::path::PathBuf,
+    std::process::ExitCode,
+};
 
 // Below are dependencies used by the API `lib.rs`, but not the CLI "main.rs".
 // However, we need to add a fake usage to suppress Cargo warnings about unused dependencies.
 // This is a known issue, and we should remove this hack once there is a better solution from Cargo.
 // https://github.com/rust-lang/cargo/issues/1982
+#[cfg(feature = "cli")]
 mod supress_api_dependencies {
     use {
         ariadne as _, metaslang_cst as _, paste as _, serde as _, strum as _, strum_macros as _,
@@ -19,6 +22,7 @@ mod supress_api_dependencies {
     };
 }
 
+#[cfg(feature = "cli")]
 #[derive(ClapParser, Debug)]
 #[command(next_line_help = true)]
 #[command(author, about)]
@@ -27,6 +31,7 @@ struct Cli {
     command: Commands,
 }
 
+#[cfg(feature = "cli")]
 #[derive(Subcommand, Debug)]
 enum Commands {
     /// Parses a Solidity (*.sol) source file, and outputs any syntax errors, or a JSON concrete syntax tree
@@ -44,6 +49,7 @@ enum Commands {
     },
 }
 
+#[cfg(feature = "cli")]
 fn main() -> Result<ExitCode> {
     match Cli::parse().command {
         Commands::Parse {
@@ -54,6 +60,7 @@ fn main() -> Result<ExitCode> {
     }
 }
 
+#[cfg(feature = "cli")]
 fn execute_parse_command(file_path_string: &str, version: Version, json: bool) -> Result<ExitCode> {
     let file_path = PathBuf::from(&file_path_string)
         .canonicalize()
@@ -84,6 +91,7 @@ fn execute_parse_command(file_path_string: &str, version: Version, json: bool) -
     }
 }
 
+#[cfg(feature = "cli")]
 #[test]
 fn verify_clap_cli() {
     // Catches problems earlier in the development cycle:
